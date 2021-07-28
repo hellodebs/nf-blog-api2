d1626fb0848e5541175c2e67aec7d1710273ccbc
hellodebs <debora.behrens@outlook.com>
2021-07-28 15:09:06 +0200

    Configure eslint

--- server/app.js
+++ server/app.js
@@ -24,6 +24,64 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/articles", (req, res) => {
+  db.findAll()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch(() => res.status(500).send("There was a mistake"));
+});
+
+app.post("/articles", (req, res) => {
+  const articles = req.body;
+  db.insert(articles).then((data) => {
+    res.status(201).send(data);
+  });
+});
+
+app.get("/articles/:id", (req, res) => {
+  const id = req.params.id;
+  db.findById(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).end();
+        return;
+      }
+
+      res.send(data);
+    })
+    .catch(() => {
+      res.status(500).send();
+    });
+});
+
+app.patch("/articles/:id", (req, res) => {
+  const id = req.params.id;
+  const content = req.body;
+  console.log(content);
+  db.updateById(id, content)
+    .then((data) => {
+      if (!data) {
+        throw "Article was not found";
+      }
+      res.send(data);
+    })
+    .catch((error) => {
+      res.status(404).send(error);
+    });
+});
+
+app.delete("/articles/:id", (req, res) => {
+  const id = req.params.id;
+  db.deleteById(id)
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch(() => {
+      res.status(500).end();
+    });
+});
+
 /*
   We have to start the server. We make it listen on the port 4000
 
