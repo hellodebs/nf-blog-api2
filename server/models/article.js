6fc8d2bf12fb199f575d25afdfee1309cfa829c1
hellodebs <debora.behrens@outlook.com>
2021-07-29 14:31:57 +0200

    Add article model

---
+++ server/models/article.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const articleSchema = new Schema(
+  {
+    title: String,
+    body: String,
+    votes: {
+      up: Number,
+      down: Number,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const Article = mongoose.model("Article", articleSchema);
+module.exports = Article;
