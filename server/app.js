10d1af35995da7b2fbb21153feef8431162f6861
hellodebs <debora.behrens@outlook.com>
2021-07-29 14:18:20 +0200

    Install mongoose and setup connection

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const db = require("./lib/db");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 /*
   We create an express app calling
@@ -12,7 +14,19 @@ const app = express();
   - parse the body of the request to json for us
   https://expressjs.com/en/guide/using-middleware.html
 */
+
+/* Time of the request, Method and URL */
+app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log("Time: ", new Date());
+  console.log("Request Type: ", req.method);
+  console.log("Url: ", req.url);
+  next();
+});
+
+/*Write a custom middleware to validate that the parameter :id  represents a number (“1” is valid, “x” is not). 
+If it’s not a number, send a 400 message back and end the request/response cycle. */
 
 /*
   Endpoint to handle GET requests to the root URI "/"
@@ -24,7 +38,24 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/articles", (req, res) => {
+function validateRequest(req, res, next) {
+  if (!req.body.title) {
+    res.status(400).json({
+      error: "Request body must contain a title property",
+    });
+    return;
+  }
+  if (!req.body.body) {
+    res.status(400).json({
+      error: "Request body must contain a body property",
+    });
+    return;
+  }
+
+  next();
+}
+
+app.get("/articles", validateRequest, (req, res) => {
   db.findAll()
     .then((data) => {
       res.send(data);
@@ -83,9 +114,21 @@ app.delete("/articles/:id", (req, res) => {
 });
 
 /*
-  We have to start the server. We make it listen on the port 4000
+  We connect to MongoDB and when the connection is successful 
+   put our express app to listen in port 4000
+  */
 
-*/
-app.listen(4000, () => {
-  console.log("Listening on http://localhost:4000");
-});
+mongoose
+  .connect("mongodb://localhost:27017/articles-api", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("conntected to mongo");
+    app.listen(4000, () => {
+      console.log("Listening on http://localhost:4000");
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
